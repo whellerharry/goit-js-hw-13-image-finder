29ed8fd6ae1daa57fbc8f033a4d0b5a8e56e0a14
whellerharry <whellerharry7@gmail.com>
2021-10-02 19:10:26 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,114 @@
 import './sass/main.scss';
+import ApiService from './js/apiService.js';
+import createImageCard from './tpl/img-card.hbs';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/countdown/dist/PNotifyCountdown.css';
+import { alert } from '@pnotify/core';
+import optNotify from './js/setNotification.js';
+import * as basicLightbox from 'basiclightbox';
+import { debounce } from 'debounce';
+import { throttle } from 'throttle-debounce';
+console.log(throttle);
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  body: document.querySelector('body'),
+  anchor: document.querySelector('.anchor'),
+  input: document.querySelector('.search-form__input'),
+};
+
+const apiService = new ApiService();
+const observer = new IntersectionObserver(throttle(500, true, observerCallback), {
+  threshold: 0,
+});
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.input.addEventListener('input', debounce(onSearch, 500));
+refs.gallery.addEventListener('click', onMakeBigImage);
+window.addEventListener('scroll', onAddObserver);
+
+function observerCallback() {
+  onLoadMore();
+}
+
+function onAddObserver() {
+  observer.observe(refs.anchor);
+}
+
+async function onSearch(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName === 'FORM') {
+    apiService.query = e.currentTarget.elements.query.value.trim();
+  }
+  if (e.target.nodeName === 'INPUT') {
+    apiService.query = e.target.value.trim();
+  }
+
+  apiService.resetPage();
+  clearGallery();
+  await fetchImages();
+
+  const fetchTotalHits = await apiService.fetchTotalHits();
+  if (fetchTotalHits === 0) {
+    return alert(optNotify.incorrectQuery);
+  }
+
+  if (apiService.query === '') {
+    helpFoundInput(e);
+    return alert(optNotify.notMachResults);
+  }
+  if (fetchTotalHits > 0) {
+    (optNotify.successResult.title = `Found ${fetchTotalHits} ${apiService.query} images`),
+      alert(optNotify.successResult);
+  }
+}
+
+async function fetchImages() {
+  if (apiService.query === '') {
+    return;
+  }
+
+  const response = await apiService.fetchImages();
+  const images = await createGallery(response);
+  return images;
+}
+
+function createGallery(images) {
+  refs.gallery.insertAdjacentHTML('beforeend', createImageCard(images));
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
+
+function onLoadMore() {
+  fetchImages();
+  console.log(apiService.query);
+  const fetchTotalHits = apiService.fetchTotalHits();
+  if (refs.gallery.children.length >= fetchTotalHits) {
+    observer.unobserve(refs.anchor);
+    alert(optNotify.imagesAreOver);
+  }
+}
+
+function onMakeBigImage(e) {
+  if (e.target.className !== 'photo-image') {
+    return;
+  }
+  const largeImagePath = e.target.dataset.large_img;
+  const instance = basicLightbox.create(`<img src="${largeImagePath}">`);
+
+  instance.show();
+}
+function helpFoundInput(e) {
+  console.log(refs.anchor.classList);
+  e.target.value = '';
+  refs.anchor.classList.toggle('pro');
+  setTimeout(() => {
+    refs.anchor.classList.toggle('pro');
+  }, 5000);
+}
+
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,7 +1,44 @@
+const API_KEY = '23668198-9ef49864e604f12f9585d3b7c';
+const BASE_URL = 'https://pixabay.com/api';
+
 export default class ApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
     }
-    
+   
+     async fetchImages() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    const response = await fetch(url);
+    const images = await response.json();
+    const hits = await images.hits;
+    this.incrementPage();
+    return hits;
+  }
+
+  async fetchTotalHits() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    const response = await fetch(url);
+    const images = await response.json();
+    const totalHits = await images.totalHits;
+    return totalHits;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
